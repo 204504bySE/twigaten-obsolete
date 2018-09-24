8a51baa9dd3c741d193f857ef7f638a7f9ccc393
204504bySE <204504byse@204504byse.info>
2018-09-24 18:55:13 +0900

    dcthash_distanceを削除したのでこっちも対応

--- twiview/dbhandler.cs
+++ twiview/dbhandler.cs
@@ -224,7 +224,7 @@ INNER JOIN tweet o ON tweet_media.tweet_id = o.tweet_id
 INNER JOIN user ou ON o.user_id = ou.user_id
 WHERE p.hash_pri IN (@dcthash,@0,@1,@2,@3,@4,@5,@6,@7,@8,@9,@10,@11,@12,@13,@14,@15,@16,@17,@18,@19,@20,@21,@22,@23,@24,@25,@26,@27,@28,@29,@30,@31,@32,@33,@34,@35,@36,@37,@38,@39,@40,@41,@42,@43,@44,@45,@46,@47,@48,@49,@50,@51,@52,@53,@54,@55,@56,@57,@58,@59,@60,@61,@62,@63)
 AND (ou.isprotected = 0 OR ou.user_id = @login_user_id OR EXISTS (SELECT * FROM friend WHERE user_id = @login_user_id AND friend_id = o.user_id))
-ORDER BY p.dcthash_distance, o.created_at LIMIT 1
+ORDER BY o.created_at LIMIT 1
 );"))
             {
                 cmd.Parameters.Add("@dcthash", MySqlDbType.Int64).Value = dcthash;
